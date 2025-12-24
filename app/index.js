ad94147226d31bde95e148b31765c21da7acd612
mobjoy0 <wessimbenna@hotmail.com>
2025-12-24 16:01:32 +0100

    Update app response to include HTTP 200 status

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.status(200).send("Hello world!");
 });
 
 app.listen(PORT, () => {
