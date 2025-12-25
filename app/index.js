8b73770bfb2bb1a349aadde34c332a21b984f26a
mobjoy0 <wessimbenna@hotmail.com>
2025-12-25 14:54:35 +0100

    Test pipeline

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hellow!");
+  res.status(200).send("Hellow oworld!");
 });
 
 app.listen(PORT, () => {
